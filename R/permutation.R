b5221f720868cda2938e358d55781caeceda88be
LotteHind <54509171+LotteHind@users.noreply.github.com>
2019-11-18 13:27:35 +0100

    Changed way of calculating p-value

--- R/permutation.R
+++ R/permutation.R
@@ -74,8 +74,12 @@ permutation <- function(groups, observations, method = "mean", nPerm = 10^5) {
 
     perm_test_values[p] <- perm_test_val
   }
-
-  p.value = (sum(abs(perm_test_values) >= abs(obs_test_stat))/nPerm)*2
+  
+  if (obs_test_stat > 0): 
+    p.value = (sum(perm_test_values >= obs_test_stat))/nPerm)*2
+  
+  if (obs_test_stat < 0):
+    p.value = (sum(perm_test_values <= obs_test_stat))/nPerm)*2
 
   if (p.value == 0) {
     warn = "Warning" = "nPerm was to low to get any permuted test statistics equal to or more extreme than your observed"
